a6727919a0a89717c74f987fc81cb7bcf385b83b
JaeHoon Lee <ljhgood9925@gmail.com>
2020-12-10 22:53:42 +0900

    /admin에서 user 변경 및 세부 속성 추가 avatar, gender, bio 모델 추가

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Create your models here.W
+
+# class User(models.Model):
+#     pass
+
+
+class User(AbstractUser):
+    """ Custom User Model """
+
+    avatar = models.ImageField(null=True)
+    # 이미지일 경우 Pillow 설치하라 나온다. pipenv install Pillow 로 설치 시도해보고
+    # 에러 나오면 버전 부분 문제로 pip로 설치한다.
+
+    gender = models.CharField(max_length=10, null=True)
+    # CharField = 한 줄
+
+    # bio = models.TextField(null=True)
+    bio = models.TextField(default="")
+    # TextField = 여러 줄
