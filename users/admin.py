944340a624fb5b5b06e0fc332985f0e84d09980b
JaeHoon Lee <ljhgood9925@gmail.com>
2020-12-11 11:20:30 +0900

    user Model 구축 완료 기존에 했던거에서 UserAdmin 클래스이용하여 확장

--- users/admin.py
+++ users/admin.py
@@ -1,23 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Custom User Admin """
 
-    #
-    list_display = (
-        "username",
-        "email",
-        "gender",
-        "language",
-        "currency",
-        "superhost",
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),  # <- 이 쉼표 해줘야함. 안하면 에러
     )
 
-    # 필터 도구
-    list_filter = ("language", "currency", "superhost")
+
+# @admin.register(models.User)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     """ Custom User Admin """
+
+#     #
+#     list_display = (
+#         "username",
+#         "email",
+#         "gender",
+#         "language",
+#         "currency",
+#         "superhost",
+#     )
+
+#     # 필터 도구
+#     list_filter = ("language", "currency", "superhost")
 
 
 # admin.site.register(models.User, CustomUserAdmin)
