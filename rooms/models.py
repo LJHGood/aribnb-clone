455e9f529546eeea6bb7e5903e5a28fddac2d77b
JaeHoon Lee <ljhgood9925@gmail.com>
2020-12-14 11:21:46 +0900

    Rooms App 마지막 복수형 이름 변경과 카메라 객체 설정 객체를 문자로 변경함

--- rooms/models.py
+++ rooms/models.py
@@ -6,7 +6,9 @@ from django_countries.fields import CountryField
 
 # 내가 만든 패키지 import
 from core import models as core_models
-from users import models as user_models
+
+# # 객체를 문자로 불러오기 때문에 임폴트 할 필요가 없음.
+# from users import models as user_models
 
 
 # Create your models here.
@@ -27,28 +29,54 @@ class AbstractItem(core_models.TimeStampedModel):
 class RoomType(AbstractItem):
     """ Roomtype Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Type"
+        # # 정렬
+        # ordering = ["created"]
+        # ordering = ["name"]
 
 
 # Amenity = 예의
 class Amenity(AbstractItem):
     """ Amenity Model Definition """
 
-    pass
+    class Meta:
+        # verbose_name_plural = 자세한 이름 복수형
+        # verbose = 말 수가 없는
+        # plural = 복수형
+        verbose_name_plural = "Amenities"
 
 
 # Facility = 시설
 class Facility(AbstractItem):
     """ Facility Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 # Rule = 규칙
 class HouseRule(AbstractItem):
     """ HouseRule Model Dfinition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    # # Room 수직하강으로 읽기 때문에 Room 객체 찾이 못함.
+    # # 이를 해결하기 위해서는 이 함수를 아래로 내리던가
+    # # 객체를 문자로 변경하면 됨.
+    # room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -68,10 +96,17 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
 
+    # 아래 객체 불러오는 것을 문자로 변경
+    # host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    # room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    # amenities = models.ManyToManyField(Amenity, blank=True)
+    # facilities = models.ManyToManyField(Facility, blank=True)
+    # house_rules = models.ManyToManyField(HouseRule, blank=True)
+
     # user 연결하는 거
     # CASCADE : 폭포수 -> 위에서 아래로 떨어지는 효과
     # 1대 다 관계이니 1이 삭제되면 같이 삭제 되는 것. on_delete=models.CASCADE
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
     # 아래 내용 core에서 만들었다
     # created = models.DateTimeField()
     # updated = models.DateTimeField()
@@ -84,13 +119,14 @@ class Room(core_models.TimeStampedModel):
     # 한 사람이 여러 객실 유형을 선택하지 않게 하기 위해서 null=True
     # 단일 선택
     # 추가, 수정, 삭제 가능(on_delete=models.SET_NULL 이걸로인해서)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
 
     # 다 대 다 연결
-    amenities = models.ManyToManyField(Amenity)
+    # 빈칸 허용(blank=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
 
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
